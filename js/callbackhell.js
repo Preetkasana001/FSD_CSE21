4a9d51968812c18683ff62403bf79fde59f4b86f
PREET <kasanapreet436@gmail.com>
2026-09-01 10:12:35 +0530

    callback functions

---
+++ js/callbackhell.js
@@ -0,0 +1,33 @@
+function test1(cb){
+    setTimeout(()=>{
+        console.log("test1");
+        cb();
+    },1000);
+
+}
+function test2(cb){
+    setTimeout(()=>{
+        console.log("test2");
+        cb();
+    },2000);
+
+}
+
+function test3(cb){
+
+    setTimeout(()=>{
+        console.log("test3");
+        cb();
+    },3000);
+
+}
+
+
+test1(()=>{
+    test2(()=>{
+        test3(()=>{
+            console.log("done");
+        })
+    });
+});
+
